182fd9309adf56bdb0ea24d347f0ec63f3e56e08
shenxuxiang <shenxuxiang@ahqianmo.com>
2024-04-08 11:30:40 +0800

    feat: 更新svg 打包构建方式

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import { reactive } from 'vue';
 import { ConfigProvider } from 'ant-design-vue';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 
-import ModelTree2 from '@/demo/ModelTree/demo2.vue';
+// import ModelTree2 from '@/demo/ModelTree/demo2.vue';
+import NotFound from '../es/assets/404.svg';
 
 const theme = reactive({
   token: { colorPrimary: '#6C69FF', colorLink: '#6C69FF' },
@@ -13,6 +14,6 @@ const theme = reactive({
 
 <template>
   <ConfigProvider :locale="zhCN" :theme="theme">
-    <ModelTree2 />
+    <NotFound width="500px" height="500px" />
   </ConfigProvider>
 </template>
